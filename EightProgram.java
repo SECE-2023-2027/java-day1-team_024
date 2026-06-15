98aca9737d3e4f5f8c3ba1858fd569df523de827
sundaresancsbs <sundaresan.mw2023csbs@sece.ac.in>
2026-06-15 21:43:27 +0530

    Eight Commit

---
+++ EightProgram.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class EightProgram {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Input a binary number: ");
+        int binary = sc.nextInt();
+
+        int decimal = 0;
+        int power = 0;
+
+        while (binary > 0) {
+            int digit = binary % 10;
+            decimal += digit * Math.pow(2, power);
+            power++;
+            binary /= 10;
+        }
+
+        System.out.println("Decimal Number: " + decimal);
+
+        sc.close();
+    }
+}
